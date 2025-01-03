f9dbd01c8c106c6e0b08e163b7977b3357237c4f
Rajani0206 <rajanisc15@gmail.com>
2025-01-03 23:45:56 +0530

    Update tagBuild.groovy

--- vars/tagBuild.groovy
+++ vars/tagBuild.groovy
@@ -3,6 +3,7 @@ def call(String tagName, String message = 'Build tagging') {
     
     // Tagging the current commit in Git
     sh """
+        git config credential.helper store //configure Git to store your username and password
         git tag -a ${tagName} -m '${message}'
         git push origin ${tagName}
     """
