d8e8840914b7340558830dcaf1096220a3b2d49c
Rajani0206 <rajanisc15@gmail.com>
2025-01-04 00:50:22 +0530

    Update tagBuild.groovy

--- vars/tagBuild.groovy
+++ vars/tagBuild.groovy
@@ -3,8 +3,7 @@ def call(String tagName, String message = 'Build tagging') {
 
    // withCredentials([usernamePassword(credentialsId: '888fe9af-1e5b-4531-b061-0686e0c595d9', usernameVariable: 'Username', passwordVariable: 'Password')]) {
         sh """
-            remote -v
-            git config credential.helper store
+            remote -v           
             git tag -a ${tagName} -m "${message}"
             git push origin ${tagName}
         """
