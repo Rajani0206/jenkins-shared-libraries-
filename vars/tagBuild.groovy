da623e0b8f5192dbe8268b99d04fa03c44545416
Rajani0206 <rajanisc15@gmail.com>
2025-01-04 00:38:28 +0530

    Update tagBuild.groovy

--- vars/tagBuild.groovy
+++ vars/tagBuild.groovy
@@ -1,7 +1,7 @@
 def call(String tagName, String message = 'Build tagging') {
     echo "Tagging the build with tag: ${tagName}"
 
-    withCredentials([usernamePassword(credentialsId: '888fe9af-1e5b-4531-b061-0686e0c595d9', usernameVariable: 'Username', passwordVariable: 'Password')]) {
+    withCredentials([usernamePassword(credentialsId: '888fe9af-1e5b-4531-b061-0686e0c595d9', usernameVariable: 'Rajani0206', passwordVariable: 'Kashvi@2211')]) {
         sh """
             
             git tag -a ${tagName} -m "${message}"
