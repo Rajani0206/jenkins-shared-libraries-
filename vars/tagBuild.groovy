905b205c210a26b9d91bcc3047ed97b2057bfb63
Rajani0206 <rajanisc15@gmail.com>
2025-01-04 00:54:24 +0530

    Update tagBuild.groovy

--- vars/tagBuild.groovy
+++ vars/tagBuild.groovy
@@ -2,9 +2,8 @@ def call(String tagName, String message = 'Build tagging') {
     echo "Tagging the build with tag: ${tagName}"
 
    // withCredentials([usernamePassword(credentialsId: '888fe9af-1e5b-4531-b061-0686e0c595d9', usernameVariable: 'Username', passwordVariable: 'Password')]) {
-        sh """
-            remote -v     
-            git remote set-url origin https://github.com/Rajani0206/Parcel-service.git
+        sh """  
+            git config remote.origin.url 
             git tag -a ${tagName} -m "${message}"
             git push origin ${tagName}
         """
