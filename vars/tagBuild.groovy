3b84f1e8930cc534268e5d613e3d32879d0bea37
Rajani0206 <rajanisc15@gmail.com>
2025-01-04 00:24:17 +0530

    Update tagBuild.groovy

--- vars/tagBuild.groovy
+++ vars/tagBuild.groovy
@@ -3,6 +3,7 @@ def call(String tagName, String message = 'Build tagging') {
     
     // Tagging the current commit in Git
     sh """
+        git remote -v
         git tag -a ${tagName} -m '${message}'
         git push origin ${tagName}
     """
