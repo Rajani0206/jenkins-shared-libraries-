edb584c51bfeeac94902f969cde99043d27d128e
Rajani0206 <rajanisc15@gmail.com>
2025-01-04 07:54:27 +0530

    Update tagBuild.groovy

--- vars/tagBuild.groovy
+++ vars/tagBuild.groovy
@@ -1,18 +1,9 @@
-def gitPushWithBuildNumber(repoUrl, branch, credentialsId, buildNumber) {
-    withCredentials([usernamePassword(credentialsId: github-credentials, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
-        try {
-            git branch: branch, url: repoUrl 
-            
-            // Create a lightweight tag
-            sh "git tag -f v${buildNumber}" 
-
-            // Push the tag to the remote repository
-            sh "git push origin v${buildNumber}" 
-        } catch (Exception e) {
-            // Handle potential errors during git operations
-            currentBuild.result = 'FAILED'
-            echo "Error pushing tag: ${e.message}"
-            throw e 
-        }
-    }
+def call(String tagName, String message = 'Build tagging') {
+    echo "Tagging the build with tag: ${tagName}"
+    
+    // Tagging the current commit in Git
+    sh """
+        git tag -a ${tagName} -m '${message}'
+        git push origin ${tagName}
+    """
 }
