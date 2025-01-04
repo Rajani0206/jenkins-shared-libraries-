7225560a1a64b7fb0f191b3de9b6945ca57bfae5
Rajani0206 <rajanisc15@gmail.com>
2025-01-04 16:49:21 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,41 +1,39 @@
-@Library('my-shared-library') _
-
-def check_out() {
+def call check_out() {
     echo 'Checking out code...'
     checkout scm
 }
 
-def setup_java() {
+def call setup_java() {
     echo 'Setting up Java 17...'
     sh 'sudo apt update'
     sh 'sudo apt install -y openjdk-17-jdk'
 }
 
-def setup_maven() {
+def call setup_maven() {
     echo 'Setting up Maven...'
     sh 'sudo apt install -y maven'
 }
 
-def build_project() {
+def call build_project() {
   echo 'Building project with Maven...'
   sh 'mvn clean package'
 }
 
-def upload_artifact(String artifactPath) {
+def call upload_artifact(String artifactPath) {
     echo 'Uploading artifact...'
     archiveArtifacts artifacts: artifactPath, allowEmptyArchive: true
 }
 
-def run_application() {
+def call run_application() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
     sleep(time: 15, unit: 'SECONDS')
 
-    def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
+    def call publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
 }
 
-def validate_app() {
+def call validate_app() {
     echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
     if (response == "200") {
@@ -46,7 +44,7 @@ def validate_app() {
     }
 }
 
-def graceful_stop() {
+def call graceful_stop() {
     echo 'Gracefully stopping the Spring Boot application...'
     sh 'mvn spring-boot:stop'
 }
